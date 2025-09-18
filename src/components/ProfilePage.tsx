c7def34c17c484d28a300e4fcf933d0abd76deac
Rega Satria Nugraha <59075378+regasatrian11@users.noreply.github.com>
2025-09-18 08:38:42 +0700

    Fix extra closing div tag

--- src/components/ProfilePage.tsx
+++ src/components/ProfilePage.tsx
@@ -483,8 +483,6 @@ export default function ProfilePage({ activeTab, onTabChange, onLogout }: Profil
             </div>
           )}
         </div>
-      </div>
-
       {/* Menu Items */}
       <div className="flex-1 overflow-y-auto p-4 space-y-2">
         <button
